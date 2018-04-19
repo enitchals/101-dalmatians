b63df97ada13c6a63b8799c031765dd4384c4a8e
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-04-19 16:42:45 -0700

    added random.js

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ In the attached data.js file, there is a list of all 101 Dalmatians. For each da
 
 `const dog = { "name": 'Pongo', "age": 3, "spots": 35, "height": 15, "weight": 24 };`
 
-This repo contains practice problems for the .map(), .reduce(), .forEach(), and .filter() array methods. You can approach this problem set in a categorical way (all the .map() problems, then .reduce(), etc), or you can approach them in a random order (forcing you to decide which method to apply in each case).
+This repo contains practice problems for the .map(), .reduce(), .forEach(), and .filter() array methods. **If you want to tackle the challenges by method, start in `sorted.js`. If you want to work on them in a random order, start in `random.js`.**
 
-Remember: these four methods are different ways of doing the same basic thing: looping through an array and doing something with each element, one by one. After you complete the exercises, challenge yourself to re-solve each problem using a different Array method.
+If you still want more practice after you complete the exercises, challenge yourself to re-solve each problem using a different Array method.
--- random.js
+++ random.js
@@ -1,3 +1,27 @@
 import dogs from './data.js';
 
-// This problem set contains the same problems as sorted.js, but in a random order. Use .map(), .reduce(), .filter(), and .forEach() to 
+// This problem set contains the same problems as sorted.js, but in a random order. Use .map(), .reduce(), .filter(), or .forEach() to solve each of the problems below.
+
+
+// Console.log() the name of each dog
+// Create an array of all the dog names
+// Create an array of dogs who are more than 15 inches tall
+// Create an array of dogs whose names start with "R" or "S"
+// How many spots do all the dogs have between them?
+// Create an array of dogs who are more than 12 inches tall
+// Create an array of all the dog names in lowercase
+// How much do all the dogs weigh, in total?
+// Create an array of the dogs' ages in dog years (One human year is equal to seven "dog years")
+// How many inches tall are all of the dogs combined?
+// Create an array of dogs who are three years old or younger
+// Create an array of dogs whose heights are 15 to 18 inches (inclusive)
+// It costs one dollar per letter to print name tags for the dogs. How many dollars will it cost to print tags for every dog?
+// Create an array of dogs who have fewer than ten spots
+// What is the total of all the dogs' ages (in human years)?
+// Create an array of dogs who have exactly one spot
+// Create an array of all the dog names spelled backwards
+// Add a "gender" property to each dog in the original array and randomly assign "M" or "F"
+// Create an array of all the dog names in uppercase
+// Create an array of dogs whose names start with a "P"
+// Create an array of dogs whose weights are 20 to 30 pounds (inclusive)
+// Console.log() "<dog> is a good boy/girl!" for each dog based on the gender property you added earlier
